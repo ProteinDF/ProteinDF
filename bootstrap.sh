9a14d09a584cf014ac2e743bf718f49369f92c58
Toshiyuki HIRANO <hiracchi@gmail.com>
2012-03-02 00:31:11 +0900

    Fix: build on MacOSX

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,15 @@ set -x
 
 rm -rf autom4te.cache
 
-libtoolize -c -f &&
+OS=`uname -s`
+
+if [ $OS == "Darwin" ]; then
+    LIBTOOLIZE=glibtoolize
+else
+    LIBTOOLIZE=libtoolize
+fi
+
+${LIBTOOLIZE} -c -f &&
 aclocal -I macros &&
 autoheader &&
 automake --foreign --add-missing --copy &&
