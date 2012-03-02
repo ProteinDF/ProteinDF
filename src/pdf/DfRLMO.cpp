07fec5648011702a9184bc78cfcc7e23ea83ca1b
Toshiyuki HIRANO <t-hirano@iis.u-tokyo.ac.jp>
2012-03-02 13:42:34 +0900

    Add: add temporary code about RLMO

--- src/pdf/DfRLMO.cpp
+++ src/pdf/DfRLMO.cpp
@@ -15,79 +15,199 @@ DfRLMO::~DfRLMO()
 void DfRLMO::exec(const std::vector<index_type>& startBlockAOs)
 {
     const int iteration = this->m_nIteration;
+    
     const TlSymmetricMatrix D_AO = this->getPpqMatrix<TlSymmetricMatrix>(RUN_RKS, iteration);
-    D_AO.save("D_AO.mtx");
+    const index_type numOfAOs = D_AO.getNumOfRows();
+    //D_AO.save("D_AO.mat");
+
+    // CD
+    {
+        TlSymmetricMatrix CD = 0.5 * D_AO;
+        std::vector<int> pivot;
+        int b = choleskyFactorization(&CD, &pivot);
+        std::cerr << "CD=" << b << std::endl;
+        CD.save("CD.mat");
+
+        TlVector p(pivot.size());
+        for (int i = 0; i < pivot.size(); ++i) {
+            p[i] = pivot[i];
+        }
+        p.save("pivot.vtr");
+
+        TlMatrix L(numOfAOs, numOfAOs);
+        for (index_type i = 0; i < numOfAOs; ++i) {
+            for (index_type j = 0; j <= i; ++j) {
+                const double v = CD.get(i, j);
+                L.set(i, j, v);
+            }
+        }
+        L.save("L0.mat");
+
+        // pivot
+        TlMatrix M(numOfAOs, numOfAOs);
+        for (index_type i = 0; i < numOfAOs; ++i) {
+            index_type target = pivot[i] -1;
+            // if (i != target) {
+                std::cerr << TlUtils::format("pivot: %d <=> %d", i, target) << std::endl;
+                TlMatrix row_part1 = L.getBlockMatrix(i,      0, 1, numOfAOs);
+                TlMatrix row_part2 = L.getBlockMatrix(target, 0, 1, numOfAOs);
+                M.setBlockMatrix(target, 0, row_part2);
+                M.setBlockMatrix(i,      0, row_part1);
+
+                TlMatrix col_part1 = L.getBlockMatrix(0, i,      numOfAOs, 1);
+                TlMatrix col_part2 = L.getBlockMatrix(0, target, numOfAOs, 1);
+                M.setBlockMatrix(0, target, col_part2);
+                M.setBlockMatrix(0, i,      col_part1);
+            // }
+        }
+        M.save("M.mat");
+
+        TlMatrix tM = M;
+        tM.transpose();
+        TlMatrix MM = M * tM;
+        MM.save("MM.mat");
+    }
+    
+    //std::cerr << this->getCMatrixPath(RUN_RKS, this->m_nIteration) << std::endl;
+    //const TlMatrix C_AO = this->getCMatrix<TlMatrix>(RUN_RKS, this->m_nIteration);
+    TlMatrix C_CMO_AO;
+    C_CMO_AO.load(this->getCMatrixPath(RUN_RKS, this->m_nIteration));
+    C_CMO_AO.save("C_CMO_AO.mat");
+
+    // {
+    //     const TlSymmetricMatrix S = this->getSpqMatrix<TlSymmetricMatrix>();
+    //     TlMatrix tC_CMO_AO = C_CMO_AO;
+    //     tC_CMO_AO.transpose();
+    //     TlMatrix CSC = tC_CMO_AO * S * C_CMO_AO;
+    //     CSC.save("CSC.mat");
+    // }
     
     // make X
     const TlSymmetricMatrix X = this->getX();
-    X.save("X.mtx");
+    // check 
+    // {
+    //     // XX == Spq
+    //     TlMatrix XX = X * X;
+    //     XX.save("XX.mtx");
+    // }
 
+    TlSymmetricMatrix Xinv = X;
+    Xinv.inverse();
+    Xinv.save("Xinv.mat");
     // check
-    {
-        TlMatrix XX = X * X;
-        XX.save("XX.mtx");
-    }
+    // {
+    //     TlMatrix XX = X * Xinv;
+    //     XX.save("XXinv.mtx");
+    // }
+        
+    // TH
+    // {
+    //     TlMatrix XC = X * C_CMO_AO;
+    //     XC.save("XC.mat");
+    // }
+
     
     const TlSymmetricMatrix D_OAO = X * D_AO * X;
-    D_OAO.save("D_OAO.mtx");
+    //D_OAO.save("D_OAO.mtx");
 
     // check
-    {
-        TlMatrix D_OAO2 = D_OAO * D_OAO;
-        D_OAO2.save("D_OAO2.mtx");
-        TlSymmetricMatrix _2D_OAO = 2.0 * D_OAO;
-        _2D_OAO.save("_2D_OAO.mtx");
-    }
+    // {
+    //     // D_OAO2 == _2D_OAO
+    //     TlMatrix D_OAO2 = D_OAO * D_OAO;
+    //     D_OAO2.save("D_OAO2.mtx");
+    //     TlSymmetricMatrix _2D_OAO = 2.0 * D_OAO;
+    //     _2D_OAO.save("_2D_OAO.mtx");
+    // }
     
     const TlMatrix T = this->getT(D_OAO, startBlockAOs);
-    T.save("T.mtx");
+    T.save("T.mat");
     TlMatrix Tt = T;
     Tt.transpose();
+    //Tt.save("Tt.mtx");
     
+    // check
+    // {
+    //     TT == E
+    //     TlMatrix TTt = T * Tt;
+    //     TTt.save("TTt.mtx");
+
+    //     TlMatrix TtT = Tt * T;
+    //     TtT.save("TtT.mtx");
+    // }
+
+  
     const TlSymmetricMatrix D_RO = Tt * D_OAO * T;
-    D_RO.save("D_RO.mtx");
+    D_RO.save("D_RO.mat");
+    
+    const TlMatrix C_CMO_RO = Tt * X * C_CMO_AO;
+    //C_CMO_RO.save("C_CMO_RO.mtx");
 
-    const TlMatrix C_AO = this->getCMatrix<TlMatrix>(RUN_RKS, this->m_nIteration);
-    const TlMatrix C_RO = Tt * X * C_AO;
-    C_RO.save("C_RO.mtx");
-
+    // TH
+    {
+        TlMatrix C_CMO_RO = Tt * X * C_CMO_AO;
+        C_CMO_RO.save("C_CMO_RO.mat");
+    }
+    
     TlMatrix U;
     {
         TlVector e;
         D_RO.diagonal(&e, &U);
+        e.save("e.vct");
+        U.save("U0.mat");
+
+        const index_type numOfRows = U.getNumOfRows();
+        const index_type numOfCols = U.getNumOfCols();
+        TlMatrix tmp(numOfRows, numOfCols);
+        for (int r = 0; r < numOfRows; ++r) {
+            for (int c = 0; c < numOfCols; ++c)  {
+                tmp.set(r, c, U.get(r, numOfCols - c -1));
+            }
+        }
+        U = tmp;
+        U.save("U.mat");
     }
+    
     TlMatrix Ut = U;
     Ut.transpose();
-    
-    TlMatrix D_RLMO = Ut * D_RO * U;
-    D_RLMO.save("D_RLMO.mtx");
-
-    TlMatrix C_RLMO = Ut * Tt * X * C_AO;
-    C_RLMO.save("C_RLMO.mtx");
-
-    TlSymmetricMatrix Xinv = X;
-    Xinv.inverse();
-    Xinv.save("Xinv.mtx");
-
-    const TlMatrix C_RLMO_AO = Xinv * T * U;
-    C_RLMO_AO.save("C_RLMO_AO.mtx");
 
     // check
-    {
-        TlMatrix C_RLMO_AO_t = C_RLMO_AO;
-        TlMatrix CC = C_RLMO_AO_t * C_RLMO;
-        CC.save("CC.mtx");
+    // {
+    //     // UU = E
+    //     TlMatrix UU = U * Ut;
+    //     UU.save("UU.mtx");
+    // }
+    
+    TlMatrix D_RLMO = Ut * D_RO * U;
+    D_RLMO.save("D_RLMO.mat");
 
-        // int numOfRows = CC.getNumOfRows();
-        // int numOfCols = CC.getNumOfCols();
-        // TlMatrix CC_(numOfRows, numOfCols);
-        // for (int c = 0; c < numOfCols; ++c) {
-        //     for (int r = 0; r <numOfRows; ++r) {
-        //         CC_.set(r, numOfCols - c -1, CC.get(r, c));
-        //     }
-        // }
-        // CC_.save("CC_.mtx");
-    }
+    TlMatrix C_CMO_RLMO = Ut * Tt * X * C_CMO_AO;
+    C_CMO_RLMO.save("C_CMO_RLMO.mat");
+    
+    const TlMatrix XinvT = Xinv * T;
+    const TlMatrix XinvTU = XinvT * U;
+    const TlMatrix& C_RLMO_AO = XinvTU;
+    XinvT.save("X_1T.mat");
+    XinvTU.save("X_1TU.mat");
+
+    // check
+    // {
+    //     TlMatrix C_RLMO_AO_t = C_RLMO_AO;
+    //     TlMatrix tCC = C_RLMO_AO_t * C_CMO_RLMO;
+    //     tCC.save("tCC.mtx");
+
+    //     // int numOfRows = CC.getNumOfRows();
+    //     // int numOfCols = CC.getNumOfCols();
+    //     // TlMatrix CC_(numOfRows, numOfCols);
+    //     // for (int c = 0; c < numOfCols; ++c) {
+    //     //     for (int r = 0; r <numOfRows; ++r) {
+    //     //         CC_.set(r, numOfCols - c -1, CC.get(r, c));
+    //     //     }
+    //     // }
+    //     // CC_.save("CC_.mtx");
+    // }
+
+    // TlMatrix C_TH = C_AO * C_RLMO_AO;
+    // C_TH.save("C_TH.mtx");
 }
 
 
@@ -109,7 +229,7 @@ TlSymmetricMatrix DfRLMO::getX()
     }
     
     TlSymmetricMatrix X = V * E * V_dagger;
-    X.save("X.mtx");
+    X.save("X.mat");
 
     return X;
 }
@@ -132,10 +252,17 @@ TlMatrix DfRLMO::getT(const TlSymmetricMatrix& D,
     for (int blockID = 0; blockID < numOfBlocks; ++blockID) {
         const index_type startAO = startBlockAOs_mod[blockID];
         const index_type endAO = startBlockAOs_mod[blockID +1];
+        std::cerr << TlUtils::format("T[%d] %d -- %d",
+                                     blockID, startAO, endAO)
+                  << std::endl;
         const index_type distance = endAO - startAO;
 
         const TlSymmetricMatrix Dsub = D.getBlockMatrix(startAO, startAO, distance, distance);
-
+        std::cerr << TlUtils::format("Dsub size = %d x %d",
+                                     Dsub.getNumOfRows(),
+                                     Dsub.getNumOfCols())
+                  << std::endl;
+        
         TlMatrix eigvec;
         TlVector eigval;
         Dsub.diagonal(&eigval, &eigvec);
