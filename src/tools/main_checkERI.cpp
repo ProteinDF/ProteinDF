c5b954571bf805e9d347e1dc2ab16887af2776a3
Toshiyuki HIRANO <hiracchi@gmail.com>
2012-05-07 17:31:24 +0900

    Fix: support ProteinDF ERI check script

--- src/tools/main_checkERI.cpp
+++ src/tools/main_checkERI.cpp
@@ -3,58 +3,48 @@
 #include <vector>
 
 #include "DfEriEngine.h"
-#include "DfTwoElectronIntegral.h"
 #include "TlGetopt.h"
 #include "TlUtils.h"
-#include "TlFile.h"
 #include "TlOrbitalInfo.h"
-#include "TlSymmetricMatrix.h"
 #include "TlMsgPack.h"
-#include "TlSerializeData.h"
-#include "TlCombineDensityMatrix.h"
+#include "TlFile.h"
 
 void showHelp()
 {
-    std::cout << "combineDensityMatrix PDF_REFERENCE_DIR PDF_TARGET_DIR [UPDATE_MATRIX_FILE]" << std::endl;
+    std::cout << "checkERI indexP indexQ indexR indexS" << std::endl;
     std::cout << "OPTIONS:" << std::endl;
-    std::cout << "  -c: check matrix" << std::endl;
-    std::cout << "  -r <range>: range" << std::endl;
-    std::cout << "  -x: disable position check" << std::endl;
-    std::cout << "  -h: show help(this)" << std::endl;
-    std::cout << "  -v: verbose" << std::endl;
+    std::cout << "  -p PDF_PARAM: ProteinDF parameter file";
+    std::cout << "  -h:           show help(this)" << std::endl;
+    std::cout << "  -v:           verbose" << std::endl;
 }
 
 
 void calc_eri(const int shellIndexP, const int shellIndexQ,
               const int shellIndexR, const int shellIndexS,
               const TlOrbitalInfo& orbitalInfo);
-void calc_eri_old(TlSerializeData* pPdfParam,
-                  const int shellIndexP, const int shellIndexQ,
-                  const int shellIndexR, const int shellIndexS,
-                  const TlOrbitalInfo& orbitalInfo);
 
-void output(int shellTypeP, int shellTypeQ,
-            int shellTypeR, int shellTypeS,
+void output(int indexP, int indexQ,
+            int indexR, int indexS,
+            const TlOrbitalInfoObject& orbitalInfo,
             const double* WORK);
-
 
 int main(int argc, char* argv[])
 {
-    TlGetopt opt(argc, argv, "hov");
+    TlGetopt opt(argc, argv, "hp:v");
 
     // parameters
     const bool isVerbose = (opt["v"] == "defined");
 
-    int shellIndexP = std::atoi(opt[1].c_str());
-    int shellIndexQ = std::atoi(opt[2].c_str());
-    int shellIndexR = std::atoi(opt[3].c_str());
-    int shellIndexS = std::atoi(opt[4].c_str());
-    std::cerr << TlUtils::format("(%d %d|%d %d)",
-                                 shellIndexP, shellIndexQ, shellIndexR, shellIndexS)
-              << std::endl;
+    int indexP = std::atoi(opt[1].c_str());
+    int indexQ = std::atoi(opt[2].c_str());
+    int indexR = std::atoi(opt[3].c_str());
+    int indexS = std::atoi(opt[4].c_str());
     
     // for reading object
-    const std::string readParamPath = "pdfparam.mpac";
+    std::string readParamPath = "pdfparam.mpac";
+    if (!opt["p"].empty()) {
+        readParamPath = opt["p"];
+    }
     TlMsgPack readMsgPack;
     if (isVerbose == true) {
         std::cout << "read parmeter file: " << readParamPath << std::endl;
@@ -68,25 +58,56 @@ int main(int argc, char* argv[])
     const TlOrbitalInfo orbitalInfo(readData["coordinates"],
                                     readData["basis_sets"]);
 
-    if (opt["o"] != "defined") {
-        calc_eri(shellIndexP, shellIndexQ, shellIndexR, shellIndexS,
-                 orbitalInfo);
-    } else {
-        std::cerr << "old engine" << std::endl;
-        calc_eri_old(&readData,
-                     shellIndexP, shellIndexQ, shellIndexR, shellIndexS,
-                     orbitalInfo);
+    const int numOfAOs = orbitalInfo.getNumOfOrbitals();
+    if (! (0 <=indexP) && (indexP < numOfAOs)) {
+        std::cerr << TlUtils::format("illegal input parameter1. %d / %d",
+                                     indexP, numOfAOs) 
+                  << std::endl;
+        std::exit(1);
     }
+    if (! (0 <=indexQ) && (indexQ < numOfAOs)) {
+        std::cerr << TlUtils::format("illegal input parameter2. %d / %d",
+                                     indexQ, numOfAOs) 
+                  << std::endl;
+        std::exit(1);
+    }
+    if (! (0 <=indexR) && (indexR < numOfAOs)) {
+        std::cerr << TlUtils::format("illegal input parameter3. %d / %d",
+                                     indexR, numOfAOs) 
+                  << std::endl;
+        std::exit(1);
+    }
+    if (! (0 <=indexS) && (indexS < numOfAOs)) {
+        std::cerr << TlUtils::format("illegal input parameter4. %d / %d",
+                                     indexS, numOfAOs) 
+                  << std::endl;
+        std::exit(1);
+    }
+
+    calc_eri(indexP, indexQ, indexR, indexS,
+             orbitalInfo);
+
+    // if (opt["o"] != "defined") {
+    // } else {
+    //     std::cerr << "old engine" << std::endl;
+    //     calc_eri_old(&readData,
+    //                  shellIndexP, shellIndexQ, shellIndexR, shellIndexS,
+    //                  orbitalInfo);
+    // }
     
     return EXIT_SUCCESS;
 }
 
 
-void calc_eri(const int shellIndexP, const int shellIndexQ,
-              const int shellIndexR, const int shellIndexS,
+void calc_eri(const int indexP, const int indexQ,
+              const int indexR, const int indexS,
               const TlOrbitalInfo& orbitalInfo)
 {
     DfEriEngine engine;
+    const int shellIndexP = orbitalInfo.getShellIndex(indexP);
+    const int shellIndexQ = orbitalInfo.getShellIndex(indexQ);
+    const int shellIndexR = orbitalInfo.getShellIndex(indexR);
+    const int shellIndexS = orbitalInfo.getShellIndex(indexS);
     const int shellTypeP = orbitalInfo.getShellType(shellIndexP);
     const int shellTypeQ = orbitalInfo.getShellType(shellIndexQ);
     const int shellTypeR = orbitalInfo.getShellType(shellIndexR);
@@ -101,62 +122,57 @@ void calc_eri(const int shellIndexP, const int shellIndexQ,
                                                           0.0);
     engine.calc(queryPQ, queryRS, PQ, RS);
 
-    output(shellTypeP, shellTypeQ,
-           shellTypeR, shellTypeS,
+    output(indexP, indexQ, indexR, indexS,
+           orbitalInfo,
            engine.WORK);
 }
 
-void calc_eri_old(TlSerializeData* pPdfParam,
-                  const int shellIndexP, const int shellIndexQ,
-                  const int shellIndexR, const int shellIndexS,
-                  const TlOrbitalInfo& orbitalInfo)
-{
-#ifdef USE_OLD_ERI_ENGINE
-    DfTwoElectronIntegral dfTwoElectronIntegral(pPdfParam);
-    DfTEI dfTEI;
 
+void output(int indexP, int indexQ,
+            int indexR, int indexS,
+            const TlOrbitalInfoObject& orbitalInfo,
+            const double* WORK)
+{
+    const int shellIndexP = orbitalInfo.getShellIndex(indexP);
+    const int shellIndexQ = orbitalInfo.getShellIndex(indexQ);
+    const int shellIndexR = orbitalInfo.getShellIndex(indexR);
+    const int shellIndexS = orbitalInfo.getShellIndex(indexS);
+    const int basisTypeP = indexP - shellIndexP;
+    const int basisTypeQ = indexQ - shellIndexQ;
+    const int basisTypeR = indexR - shellIndexR;
+    const int basisTypeS = indexS - shellIndexS;
     const int shellTypeP = orbitalInfo.getShellType(shellIndexP);
     const int shellTypeQ = orbitalInfo.getShellType(shellIndexQ);
     const int shellTypeR = orbitalInfo.getShellType(shellIndexR);
     const int shellTypeS = orbitalInfo.getShellType(shellIndexS);
-    const unsigned int eriType = DfTEI::getEriType(shellTypeP, shellTypeQ, shellTypeR, shellTypeS);
-    dfTwoElectronIntegral.prepare_ERI();
-    const DfTEI::ShellPair IJ = dfTwoElectronIntegral.getShellPair(shellIndexP, shellIndexQ);
-    const DfTEI::ShellPair KL = dfTwoElectronIntegral.getShellPair(shellIndexR, shellIndexS);
-
-    
-    dfTEI.calc(eriType, IJ, KL);
-
-    output(shellTypeP, shellTypeQ,
-           shellTypeR, shellTypeS,
-           dfTEI.ERI);
-#endif // USE_OLD_ERI_ENGINE
-}
-
-void output(int shellTypeP, int shellTypeQ,
-            int shellTypeR, int shellTypeS,
-            const double* WORK)
-{
     const int maxStepsP = 2 * shellTypeP + 1;
     const int maxStepsQ = 2 * shellTypeQ + 1;
     const int maxStepsR = 2 * shellTypeR + 1;
     const int maxStepsS = 2 * shellTypeS + 1;
-    // const int maxStepsP = shellTypeP * (shellTypeP + 3) / 2 + 1;
-    // const int maxStepsQ = shellTypeQ * (shellTypeQ + 3) / 2 + 1;
-    // const int maxStepsR = shellTypeR * (shellTypeR + 3) / 2 + 1;
-    // const int maxStepsS = shellTypeS * (shellTypeS + 3) / 2 + 1;
-    int index = 0;
-    for (int i = 0; i < maxStepsP; ++i) {
-        for (int j = 0; j < maxStepsQ; ++j) {
-            for (int k = 0; k < maxStepsR; ++k) {
-                for (int l = 0; l < maxStepsS; ++l) {
-                    const double value = WORK[index];
-                    std::cout << TlUtils::format("(%2d %2d|%2d %2d)=%18.10f",
-                                                 i, j, k, l, value)
-                              << std::endl;
-                    ++index;
+    
+    {
+        int index = 0;
+        for (int i = 0; i < maxStepsP; ++i) {
+            const int P = shellIndexP + i;
+            for (int j = 0; j < maxStepsQ; ++j) {
+                const int Q = shellIndexQ + j;
+                for (int k = 0; k < maxStepsR; ++k) {
+                    const int R = shellIndexR + k;
+                    for (int l = 0; l < maxStepsS; ++l) {
+                        const int S = shellIndexS + l;
+                        const double value = WORK[index];
+                        std::cout << TlUtils::format("[%d %d|%d %d] -> % f", P, Q, R, S, value)
+                                  << std::endl;
+                        ++index;
+                    }
                 }
             }
         }
     }
+
+    const int index = ((basisTypeP * maxStepsQ + basisTypeQ) * maxStepsR + basisTypeR) * maxStepsS + basisTypeS;
+    const double value = WORK[index];
+    std::cout << TlUtils::format("(%2d %2d|%2d %2d)=%18.10f",
+                                 indexP, indexQ, indexR, indexS, value)
+              << std::endl;
 }
---
+++ src/tools/main_pdfOrbInfo.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+#include "TlGetopt.h"
+#include "TlOrbitalInfo.h"
+#include "TlMsgPack.h"
+#include "TlSerializeData.h"
+#include "TlPseudoYaml.h"
+
+#define AU_BOHR 1.889762
+
+const char shellTypes[] = {'s', 'p', 'd'};
+const char* angularMomentumTypes[] = {
+    "s",
+    "px", "py", "pz",
+    "dxy", "dyz", "dzx", "dxx-yy", "dzz"
+};
+
+
+void printOrbInfo(const TlOrbitalInfo& tlOrbInfo,
+                  std::size_t orb)
+{
+    const TlPosition pos = tlOrbInfo.getPosition(orb);
+    std::cout << TlUtils::format("%d th orbital information:", orb)
+              << std::endl;
+    std::cout << TlUtils::format(" atom: index=%d, symbol=%s, position(% 8.3f, % 8.3f, % 8.3f)",
+                                 tlOrbInfo.getAtomIndex(orb),
+                                 tlOrbInfo.getAtomName(orb).c_str(),
+                                 pos.x() / AU_BOHR, pos.y() / AU_BOHR, pos.z() / AU_BOHR)
+              << std::endl;
+    std::cout << TlUtils::format(" orbital: type=%c(%s)",
+                                 shellTypes[tlOrbInfo.getShellType(orb)],
+                                 angularMomentumTypes[tlOrbInfo.getBasisType(orb)])
+              << std::endl;
+    const int contractions = tlOrbInfo.getCgtoContraction(orb);
+    std::cout << TlUtils::format("          contraction=%d",
+                                 contractions)
+              << std::endl;
+    for (int j = 0; j < contractions; ++j) {
+        std::cout << TlUtils::format("          coef=%e, exp=%e",
+                                     tlOrbInfo.getCoefficient(orb, j),
+                                     tlOrbInfo.getExponent(orb, j))
+                  << std::endl;
+    }
+}
+
+
+void showHelp()
+{
+    std::cout << "pdfOrbInfo [OPTIONS] index ..." << std::endl;
+    std::cout << " display orbital information." << std::endl;
+    std::cout << " -a:           Atom index mode: index means atom serial number." << std::endl;
+    std::cout << " -p PDF_param: ProteinDF parameter file (default: pdfparam.mpac)" << std::endl;
+    std::cout << " -w mpac_file: output MessagaPack file" << std::endl;
+    std::cout << " -v:           verbose" << std::endl;
+       
+}
+
+
+int main(int argc, char* argv[])
+{
+    TlGetopt opt(argc, argv, "ahp:vw:");
+    const bool isAtomIndexMode = (opt["a"] == "defined");
+    const bool isVerbose = (opt["v"] == "defined");
+    const bool isShowHelp = (opt["h"] == "defined");
+    std::string pdfparamPath = "pdfparam.mpac";
+    if (opt["p"].empty() != true) {
+        pdfparamPath = opt["p"];
+    }
+    std::string saveMpacPath = "";
+    if (opt["w"].empty() != true) {
+        saveMpacPath = opt["w"];
+    }
+
+    if (isShowHelp) {
+        showHelp();
+        std::exit(0);
+    }
+
+    TlMsgPack mpac;
+    if (isVerbose) {
+        std::cerr << TlUtils::format("reading %s ...", pdfparamPath.c_str());
+    }
+    mpac.load(pdfparamPath);
+    TlSerializeData data = mpac.getSerializeData();
+    TlOrbitalInfo orbInfo(data["coordinates"], data["basis_sets"]);
+
+    const std::size_t numOfAOs = orbInfo.getNumOfOrbitals();
+    if (isVerbose == true) {
+        std::cerr << "total atoms: " << orbInfo.getNumOfAtoms() << std::endl;
+        std::cerr << "total orbitals: " << numOfAOs << std::endl;
+    }
+
+    const int numOfArgs = opt.getCount() -1; // opt[0] means the name of this program.
+    if (numOfArgs == 0) {
+        showHelp();
+        std::exit(1);
+    }
+
+    if (isAtomIndexMode == true) {
+        for (int i = 0; i < numOfArgs; ++i) {
+            const int atomIndex = std::atoi(opt[i +1].c_str());
+            
+            std::cout << TlUtils::format(">>>> atom index: %d", atomIndex) << std::endl;
+            for (int aoIndex = 0; aoIndex < numOfAOs; ++aoIndex) {
+                if (atomIndex == orbInfo.getAtomIndex(aoIndex)) {
+                    printOrbInfo(orbInfo, aoIndex);
+                }
+            }
+        }
+    } else {
+        if (saveMpacPath.empty()) {
+            for (int i = 0; i < numOfArgs; ++i) {
+                const std::size_t orb = std::atol(opt[i +1].c_str());
+                if (orb < numOfAOs) {
+                    printOrbInfo(orbInfo, orb);
+                }
+            }
+        } else {
+            TlSerializeData output;
+            for (int i = 0; i < numOfArgs; ++i) {
+                const int orb = std::atoi(opt[i +1].c_str());
+                if (orb < numOfAOs) {
+                    TlSerializeData entry;
+
+                    entry["atom_index"] = orbInfo.getAtomIndex(orb);
+                    entry["atomic_number"] = TlAtom::getElementNumber(orbInfo.getAtomName(orb));
+                    entry["symbol"] = orbInfo.getAtomName(orb);
+                    const TlPosition pos = orbInfo.getPosition(orb);
+                    TlSerializeData xyz;
+                    xyz.pushBack(pos.x() / AU_BOHR);
+                    xyz.pushBack(pos.y() / AU_BOHR);
+                    xyz.pushBack(pos.z() / AU_BOHR);
+                    entry["xyz"] = xyz;
+
+                    entry["shell_type_id"] = orbInfo.getShellType(orb);
+                    entry["shell_type"] = TlUtils::format("%c", shellTypes[orbInfo.getShellType(orb)]);
+                    entry["basis_type_id"] = orbInfo.getBasisType(orb);
+                    entry["basis_type"] = std::string(angularMomentumTypes[orbInfo.getBasisType(orb)]);
+                    
+                    const int contractions = orbInfo.getCgtoContraction(orb);
+                    for (int c = 0; c < contractions; ++c) {
+                        TlSerializeData pGTO;
+                        pGTO["exp"] = orbInfo.getExponent(orb, c);
+                        pGTO["coef"] = orbInfo.getCoefficient(orb, c);
+                        entry["pGTOs"].pushBack(pGTO);
+                    }
+
+                    output[orb] = entry;
+                }
+            }
+            
+            TlMsgPack mpack(output);
+            if (isVerbose) {
+                std::cerr << TlUtils::format("output: %s", saveMpacPath.c_str()) << std::endl;
+            }
+            mpack.save(saveMpacPath);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
