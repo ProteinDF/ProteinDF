e4d2434e7bd0da8b8e0cd59eaa612a682ed27e95
Toshiyuki HIRANO <hiracchi@gmail.com>
2012-07-24 15:39:44 +0900

    Add: cache template class

---
+++ include/TlSharedPointer.h
@@ -0,0 +1,121 @@
+#ifndef TLSHAREDPOINTER_H
+#define TLSHAREDPOINTER_H
+
+#include <cassert>
+
+/** リファレンスカウント式Smart Pointer
+ *
+ *  - newで作成したポインタのみ保持させること。new 以外で生成したポインタは解放できない。
+ *  - newで作成したポインタを複数のTlSharedPoinnterに渡さないこと(2重解放になる)。
+ *  - 配列のポインタを保持させないこと(delete []を行わないため)。
+ */
+template<typename T>
+class TlSharedPointer {
+public:
+    TlSharedPointer(T* pObject);
+    TlSharedPointer(const TlSharedPointer<T>& psp);
+
+    ~TlSharedPointer();
+
+    TlSharedPointer<T>& operator=(const TlSharedPointer<T>& sp);
+
+    bool operator==(const T* p);
+    bool operator!=(const T* p);
+
+    T* operator->() const;
+    T& operator*() const;
+
+private:
+    void set(const TlSharedPointer<T>& psp);
+    void release();
+
+private:
+    T* m_pObject;
+    int* m_pCount;
+
+    const static T* m_pNullObject; // NULLポインタ
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+const T* TlSharedPointer<T>::m_pNullObject = NULL;
+
+template<typename T>
+TlSharedPointer<T>::TlSharedPointer(T* pObject) {
+    this->m_pObject = pObject;
+
+    this->m_pCount = new int;
+    *(this->m_pCount) = 1;
+}
+
+template<typename T>
+TlSharedPointer<T>::TlSharedPointer(const TlSharedPointer<T>& psp) {
+    this->m_pObject = NULL;
+    this->m_pCount = NULL;
+
+    this->set(psp);
+}
+
+template<typename T>
+TlSharedPointer<T>::~TlSharedPointer() {
+    this->release();
+}
+
+template<typename T>
+TlSharedPointer<T>& TlSharedPointer<T>::operator=(const TlSharedPointer<T>& psp) {
+    this->set(psp);
+    return (*this);
+}
+
+template<typename T>
+bool TlSharedPointer<T>::operator==(const T* p) {
+    if (this->m_pObject == p) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template<typename T>
+bool TlSharedPointer<T>::operator!=(const T* p) {
+    return !(this->operator==(p));
+}
+
+template<typename T>
+T* TlSharedPointer<T>::operator->() const {
+    return this->m_pObject;
+}
+
+template<typename T>
+T& TlSharedPointer<T>::operator*() const {
+    return *(this->m_pObject);
+}
+
+template<typename T>
+void TlSharedPointer<T>::set(const TlSharedPointer<T>& psp){
+    if (this != &psp){
+        this->release();
+
+        this->m_pObject = psp.m_pObject;
+        this->m_pCount = psp.m_pCount;
+        ++(*(this->m_pCount));
+    }
+}
+
+template<typename T>
+void TlSharedPointer<T>::release(){
+    if (this->m_pCount != NULL){
+        --(*(this->m_pCount));
+
+        if (*(this->m_pCount) == 0){
+            delete this->m_pObject;
+            delete this->m_pCount;
+        }
+    }
+
+    this->m_pObject = NULL;
+    this->m_pCount = NULL;
+}
+
+#endif // TLSHAREDPOINTER_H
--- include/TlStlUtils.h
+++ include/TlStlUtils.h
@@ -1,10 +1,14 @@
 #ifndef TLSTLUTILS_H
 #define TLSTLUTILS_H
 
+#include <list>
+#include <map>
+#include "TlSharedPointer.h"
+
 class TlStlUtils {
 public:
     template<typename MapType, typename KeyArgType, typename ValueArgType>
-    static typename MapType::iterator efficientAddorUpdate(MapType& m,
+    static typename MapType::iterator efficientAddOrUpdate(MapType& m,
                                                            const KeyArgType& k,
                                                            const ValueArgType& v);
 };
@@ -12,9 +16,9 @@ public:
 
 // see Effective STL 3-24
 template<typename MapType, typename KeyArgType, typename ValueArgType>
-typename MapType::iterator efficientAddorUpdate(MapType& m,
-                                                const KeyArgType& k,
-                                                const ValueArgType& v)
+typename MapType::iterator TlStlUtils::efficientAddOrUpdate(MapType& m,
+                                                            const KeyArgType& k,
+                                                            const ValueArgType& v)
 {
     typename MapType::iterator lb = m.lower_bound(k);
     if (lb != m.end() &&
@@ -26,5 +30,64 @@ typename MapType::iterator efficientAddorUpdate(MapType& m,
         return m.insert(lb, MVT(k, v));
     }
 }
+
+
+template<typename KeyType, typename DataType, typename KeyCompare=std::less<KeyType> >
+class TlCache {
+private:
+    typedef std::list<KeyType> KeyList;
+    typedef typename std::list<KeyType>::iterator KeyListItr;
+    typedef typename std::pair<TlSharedPointer<DataType>, KeyListItr> CacheType;
+    typedef std::map<KeyType, CacheType, KeyCompare> CacheTable;
+
+public:
+    TlCache(std::size_t cacheLimit = 1024 * 1024);
+    ~TlCache();
+
+public:
+    void set(const KeyType& key, const DataType& data) {
+        typename CacheTable::iterator pCache = this->cacheTbl_.lower_bound(key);        
+        if ((pCache != this->cacheTbl_.end()) &&
+            (!(this->cacheTbl_.key_comp()(key, pCache->first)))) {
+            pCache->second = TlSharedPointer(new DataType(data));
+        } else {
+            CacheType cacheData = std::make_pair(TlSharedPointer(new DataType(data)),
+                                                 this->reqHistory_.insert(this->reqHistory_.begin(), key));
+            this->cacheTbl_.insert(pCache, std::make_pair(key, cacheData));
+        }
+    }
+
+    const TlSharedPointer<DataType>& get(const KeyType& key) {
+        KeyListItr reqHistoryHead = this->reqHistory_.begin();
+        typename CacheTable::iterator pCache = this->cacheTbl_.lower_bound(key);
+        if (pCache != this->cacheTbl_.end()) {
+            // cache hit!
+            if (this->cacheTbl_.key_comp()(key, pCache->first) == false) {
+                this->reqHistory_.splice(reqHistoryHead,
+                                         this->reqHistory, pCache->second.second);
+            }
+            return pCache->second.first;
+        } else {
+            // create new data
+
+            // 古いデータを消す
+            while (this->cacheTbl_.size() > this->cacheLimit_) {
+                KeyListItr reqHistoryTail = --(this->reqHistory_.end());
+                this->cacheTbl_.erase(*reqHistoryTail);
+                this->reqHistory_.erase(reqHistoryTail);
+            }
+
+            // 
+            CacheType cacheData = std::make_pair(TlSharedPointer(new DataType()),
+                                                 this->reqHistory_.insert(reqHistoryHead, key));
+            this->cacheTbl_.insert(pCache, std::make_pair(key, cacheData));
+            return cacheData.first;
+        }
+    }
+
+private:
+    CacheTable cacheTbl_;
+    KeyList reqHistory_;
+};
 
 #endif // TLSTLUTILS_H
