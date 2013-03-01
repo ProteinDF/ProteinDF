b2706cf0a9b9fc1fd24904708c59ba1d0aad4413
Toshiyuki HIRANO <hiracchi@gmail.com>
2013-03-01 14:12:44 +0900

    Fix: pdf-kwd help

--- src/tools/main_kwd.cpp
+++ src/tools/main_kwd.cpp
@@ -22,24 +22,24 @@ int main(int argc, char* argv[])
     if (isCSV == true) {
         std::string csv = kwd.getCSV(showAll);
         std::cout << csv << std::endl;
+    } else if (mpacFilePath.empty() != true) {
+        const TlSerializeData data = kwd.getSerializeData();
+        TlMsgPack mpac(data);
+        mpac.save(mpacFilePath);
     } else {
         kwd.printDefault(std::cout);
     }
 
-    if (mpacFilePath.empty() != true) {
-        const TlSerializeData data = kwd.getSerializeData();
-        TlMsgPack mpac(data);
-        mpac.save(mpacFilePath);
-    }
-    
     return EXIT_SUCCESS;
 }
 
 
 void usage()
 {
-    std::cout << "calculation Mulliken Population." << std::endl;
-    std::cout << "usage: " << std::endl;
-    std::cout << "prog [options] P_matrix_path S_matrix_path" << std::endl;
+    std::cout << "output ProteinDF keyword" << std::endl;
+    std::cout << std::endl;
+    std::cout << "  -a:      output hidden parameters" << std::endl;
+    std::cout << "  -c:      output CSV format" << std::endl;
+    std::cout << "  -m FILE: output MsgPack file" << std::endl;
 }
 
