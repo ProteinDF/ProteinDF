2b8c5cd62101afce5959352e9d6f4451d7a6dfaa
Toshiyuki HIRANO <hiracchi@gmail.com>
2013-04-01 23:31:30 +0900

    Fix: pdf-kwdで日本語, ReSTが使えるようにした

--- src/tools/main_kwd.cpp
+++ src/tools/main_kwd.cpp
@@ -12,16 +12,27 @@ void usage();
 
 int main(int argc, char* argv[])
 {
-    TlGetopt opt(argc, argv, "acjm:");
+    TlGetopt opt(argc, argv, "acjm:r");
     const bool showAll = (opt["a"] == "defined");
     const bool isCSV = (opt["c"] == "defined");
+    const bool isJP = (opt["j"] == "defined");
+    const bool is_reST = (opt["r"] == "defined");
     const std::string mpacFilePath = opt["m"];
 
     PdfKeyword kwd;
 
     if (isCSV == true) {
-        std::string csv = kwd.getCSV(showAll);
+        std::string csv;
+        if (isJP == true) {
+            csv = kwd.getCSV_jp(showAll);
+        } else {
+            csv = kwd.getCSV(showAll);
+        } 
         std::cout << csv << std::endl;
+    } else if (is_reST == true) {
+        std::string reST;
+        reST = kwd.get_reST_jp(showAll);
+        std::cout << reST << std::endl;
     } else if (mpacFilePath.empty() != true) {
         const TlSerializeData data = kwd.getSerializeData();
         TlMsgPack mpac(data);
@@ -40,6 +51,8 @@ void usage()
     std::cout << std::endl;
     std::cout << "  -a:      output hidden parameters" << std::endl;
     std::cout << "  -c:      output CSV format" << std::endl;
+    std::cout << "  -r:      output ReST format" << std::endl;
+    std::cout << "  -j:      output Japanese" << std::endl;
     std::cout << "  -m FILE: output MsgPack file" << std::endl;
 }
 
