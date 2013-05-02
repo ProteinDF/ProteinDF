94fbfb8f979da30dc29cddd25c34dfc4091f7298
Toshiyuki HIRANO <hiracchi@gmail.com>
2013-05-02 15:15:20 +0900

    Add: old source to stock

---
+++ stock/Fl_Tbl_Orbital.cxx
@@ -0,0 +1,214 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <cassert>
+
+#include "Fl_Tbl_Orbital.h"
+#include "FileX.h"
+#include "TlUtils.h"
+#include "CnError.h"
+
+const std::string Fl_Tbl_Orbital::S_ORB_STR[] = {"s()"};
+const std::string Fl_Tbl_Orbital::P_ORB_STR[] = {"p(x)", "p(y)", "p(z)"};
+const std::string Fl_Tbl_Orbital::D_ORB_STR[] = {"d(xy)", "d(xz)", "d(yz)", "d(x2-y2)", "d(3z2-r2)"};
+
+int Fl_Tbl_Orbital::flag1=0;        // flag for Constructor;
+// first : flag1=0; after : flag1=1;
+int Fl_Tbl_Orbital::cGtoTotalNum=0; // s*1,p*3,d*5;
+
+Fl_Tbl_Orbital::Fl_Tbl_Orbital(const Fl_Geometry& flGeom,
+                               const std::string& path) : flGeom_(flGeom), m_sTableFilePath(path)
+{
+    if (flag1 == 0) {
+        this->prepare();
+
+        if (FileX::isExist(this->m_sTableFilePath) == false) {
+            this->makeTable();
+        } else {
+            this->setData();
+        }
+    } else {
+        this->setData();
+    }
+}
+
+Fl_Tbl_Orbital::~Fl_Tbl_Orbital()
+{
+}
+
+void Fl_Tbl_Orbital::prepare()
+{
+    //Fl_Geometry FlGeom(Fl_Geometry::getDefaultFileName());
+    Fl_Gto_Orbital FlGtoOrb;
+
+    AtomNum = this->flGeom_.getNumOfAtoms();
+    AtomKindNum = this->flGeom_.getAtomKindNumber();
+
+    cGtoTotalNum = 0;
+
+    for (int i = 0; i < AtomNum; ++i) {
+        std::string Atm = this->flGeom_.getAtom(i);
+        std::string Lb2 = this->flGeom_.getLabel(i);
+
+        for (int j = 0; j < AtomKindNum; ++j) {
+            int m = FlGtoOrb.getStartposition(j);
+            if (m < 0) {
+                //std::cerr << "[TH] !(m >= 0) at Fl_Tbl_Orbital::prepare()." << std::endl;
+                continue;
+            }
+
+            if ((Atm == FlGtoOrb.getAtom(m)) &&
+                    (Lb2 == FlGtoOrb.getLabel(m))) {
+                const int start = m;
+                const int to = m + FlGtoOrb.getTermnumber(j);
+                for (int k = start; k < to; ++k) {
+                    const char shell = FlGtoOrb.getShell(k);
+                    int iter = 0;
+                    switch (shell) {
+                    case 's':
+                        iter = 1;
+                        break;
+                    case 'p':
+                        iter = 3;
+                        break;
+                    case 'd':
+                        iter = 5;
+                        break;
+                    default:
+                        CnErr.abort(TlUtils::format("program error. %s: %d\n", __FILE__, __LINE__));
+                    }
+
+                    this->cGtoTotalNum += iter;
+                }
+                break;
+            }
+        }
+    }
+}
+
+void Fl_Tbl_Orbital::makeTable()
+{
+    //const Fl_Geometry FlGeom((*this->pPdfParam_)["coordinates"]);
+    Fl_Gto_Orbital FlGtoOrb;
+
+    const int nAtomNum = this->flGeom_.getNumOfAtoms();
+    const int nAtomKindNum = this->flGeom_.getAtomKindNumber();
+
+    std::ofstream fo;
+    fo.open(Fl_Tbl_Orbital::m_sTableFilePath.c_str(), std::ios::out | std::ios::trunc);
+    if (!fo) {
+        CnErr.abort(TlUtils::format("Cannot Open %s\n", Fl_Tbl_Orbital::m_sTableFilePath.c_str()));
+    }
+
+    int basiscount = 0;
+    for (int i = 0; i < nAtomNum; ++i) {
+        const std::string Atm = this->flGeom_.getAtom(i);
+        const std::string Lb2 = this->flGeom_.getLabel(i);
+
+        //std::cout << "Atm=\"" << Atm << "\", Lb2=\"" << Lb2 << "\"" << std::endl;
+
+        for (int j = 0; j < nAtomKindNum; ++j) {
+            int m = FlGtoOrb.getStartposition(j);
+            if ((Atm == FlGtoOrb.getAtom(m)) &&
+                    (Lb2 == FlGtoOrb.getLabel(m))) {
+                const int start = m;
+                const int to = m + FlGtoOrb.getTermnumber(j);
+                for (int k = start; k < to; ++k) {
+                    const int cGtonum = k;
+                    const char shell = FlGtoOrb.getShell(k);
+                    int iter = 0;
+                    switch (shell) {
+                    case 's':
+                        iter = 1;
+                        break;
+                    case 'p':
+                        iter = 3;
+                        break;
+                    case 'd':
+                        iter = 5;
+                        break;
+                    default:
+                        CnErr.abort(TlUtils::format("make table error. @ %s:%d\n",__FILE__, __LINE__));
+                        break;
+                    }
+
+                    int rept = 0;
+                    for (int t = 0; t < iter; ++t) {
+                        fo << std::setw(8) <<  basiscount
+                        << std::setw(6) <<  cGtonum
+                        << std::setw(6) <<  i
+                        << std::setw(6) <<  Atm
+                        << "    ";
+                        basiscount++;
+                        switch (shell) {
+                        case 's':
+                            fo <<" s    "
+                            << std::setw(10) << Fl_Tbl_Orbital::S_ORB_STR[rept]
+                            <<"   ["<< Lb2 <<"]"<< "\n";
+                            break;
+                        case 'p':
+                            fo <<" p    "
+                            << std::setw(10) << Fl_Tbl_Orbital::P_ORB_STR[rept]
+                            <<"   ["<< Lb2 <<"]"<< "\n";
+                            break;
+                        case 'd':
+                            fo <<" d    "
+                            << std::setw(10) << Fl_Tbl_Orbital::D_ORB_STR[rept]
+                            <<"   ["<< Lb2 <<"]"<< "\n";
+                            break;
+                        default:
+                            CnErr.abort(TlUtils::format("make table error. @ %s:%d\n",__FILE__, __LINE__));
+                            break;
+                        }
+                        rept++;
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+
+    fo.close();
+
+    //  this->setData();
+}
+
+void Fl_Tbl_Orbital::setData()
+{
+    std::ifstream ifs;
+    ifs.open(this->m_sTableFilePath.c_str(), std::ios::in);
+    if (!ifs) {
+        CnErr.abort(TlUtils::format("Cannot open %s\n", Fl_Tbl_Orbital::m_sTableFilePath.c_str()));
+    }
+
+    int nBasisCount;
+    //for (int intI = 0; intI < cGtoTotalNum; intI++){
+    std::string sLine;
+    while ((ifs) && (std::getline(ifs, sLine))) {
+        std::istringstream iss(sLine);
+        BasisTable basis;
+        iss >> nBasisCount
+        >> basis.cGtonum
+        >> basis.InpAtomnumber
+        >> basis.Atom
+        >> basis.shell
+        >> basis.dumy1
+        >> basis.dumy2;
+
+        this->m_BasisTable.push_back(basis);
+    }
+
+    this->cGtoTotalNum = this->m_BasisTable.size();
+
+    ifs.close();
+}
+
+
+
+
+
+
+
---
+++ stock/Fl_Tbl_Xcpot2.cxx
@@ -0,0 +1,327 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cassert>
+
+#include "Fl_Tbl_Xcpot2.h"
+#include "FileX.h"
+#include "TlLogging.h"
+#include "CnError.h"
+
+int  Fl_Tbl_Xcpot2::flag1=0;         // flag for Constructor;
+// first : flag1=0; after : flag1=1;
+int  Fl_Tbl_Xcpot2::flag2=0;         // flag for Destructor;
+// notGetmemory : flag2=0;
+// getMemory    : flag2=1;
+int Fl_Tbl_Xcpot2::cGtoTotalNum=0; // s*1,p*3,d*5;
+
+//#######################################################################
+Fl_Tbl_Xcpot2::Fl_Tbl_Xcpot2(const Fl_Geometry& flGeom)
+    : flGeom_(flGeom) {
+    TlLogging& log = TlLogging::getInstance();
+//   std::cerr << "Fl_Tbl_Xcpot2::Fl_Tbl_Xcpot2() constructer" << std::endl;
+
+    int memory;
+
+    flag2=0;
+    if (flag1==0) {
+        prepare();
+        memory = Estmemval();
+        if (memory > MaxFlBasisTblmemory) {
+            log.error("Cannot get memory !!(in Fl_Tbl_Xcpot2.cxx no constractor)");
+            log.error("Used Memory is " + TlUtils::xtos(8 * memory) + " [byte] ");
+            CnErr.abort();
+        } else {
+//      Log <<"Used Memory is " << 8 * memory <<" [byte] " <<"\n";
+            getMemory();
+
+            if (FileX::isExist("fl_Table/XcpotTable2") == false) {
+                makeTable();
+            } else {
+                this->setData();
+            }
+
+            flag2=1;
+        }
+        flag1=1;
+    } else {
+        getMemory();
+        setData();
+    }
+}
+
+Fl_Tbl_Xcpot2::~Fl_Tbl_Xcpot2()
+{
+    int intI;
+    for (intI=0; intI<cGtoTotalNum; intI++) {
+        delete[] BsTbl[intI].Atom ;
+        delete[] BsTbl[intI].shell;
+        delete[] BsTbl[intI].dumy1;
+        delete[] BsTbl[intI].dumy2;
+    }
+
+    delete[] BsTbl;
+}
+
+//#######################################################################
+int Fl_Tbl_Xcpot2::prepare()
+{
+    TlLogging& log = TlLogging::getInstance();
+
+    int i,j,k,m,start,to,flag;
+    char *Atm = new char[MaxAtomName];
+    char *Lb2 = new char[MaxLabelname];
+    char shell;
+    //const Fl_Geometry FlGeom((*this->pPdfParam_)["coordinates"]);
+    Fl_Gto_Xcpot2      FlGtoXcpot2;
+
+//   FlGeom.load();
+//   FlGeom.open("fl_Geometry","read");
+//   FlGeom.read();
+// //FlGeom.show();
+//   FlGeom.close();
+
+///// modified by Fric(mizouchi) 2001/12/09 (s) /////
+//    FlGtoXcpot2.open("fl_Gto_Xcpot2","read");
+//   FlGtoXcpot2.open("fl_Gto_Xcpot","read");
+///// modified by Fric(mizouchi) 2001/12/09 (e) /////
+
+//   FlGtoXcpot2.read();
+//FlGtoXcpot2.show();
+//   FlGtoXcpot2.close();
+
+    AtomNum     = this->flGeom_.getNumOfAtoms();
+    AtomKindNum = this->flGeom_.getAtomKindNumber();
+
+    cGtoTotalNum=0;
+
+    for (i=0; i<AtomNum; i++) {
+        flag=0;
+        std::strcpy(Atm, this->flGeom_.getAtom(i).c_str());
+        std::strcpy(Lb2, this->flGeom_.getLabel(i).c_str());
+        for (j=0; j<AtomKindNum; j++) {
+            m = FlGtoXcpot2.getStartposition(j);
+            if ((strcmp(Atm,FlGtoXcpot2.getAtom(m).c_str())==0)  &&
+                    (strcmp(Lb2,FlGtoXcpot2.getLabel(m).c_str())==0)) {
+                start = m;
+                to    = m + FlGtoXcpot2.getTermnumber(j);
+                for (k=start; k<to; k++) {
+                    shell   = FlGtoXcpot2.getShell(k);
+                    int iter = 0;
+                    switch (shell) {
+                    case 's' :
+                        iter=1;
+                        break;
+                    case 'p' :
+                        iter=3;
+                        break;
+                    case 'd' :
+                        iter=5;
+                        break;
+                    default  :
+                        log.error("Error (making Table for Yahito(Xcot)))");
+                        CnErr.abort();
+                    }
+
+                    cGtoTotalNum += iter;
+                }
+                flag=1;
+            }
+            if (flag==1) break;
+        }
+    }
+
+    delete[] Atm;
+    delete[] Lb2;
+
+    return 0;
+}
+//#######################################################################
+int Fl_Tbl_Xcpot2::Estmemval(void)
+{
+    int EstMemory;
+
+    EstMemory =  sizeof(BasisTable*) * cGtoTotalNum
+                 + sizeof(char*) * MaxLabelname *4     ;
+
+    return EstMemory;
+}
+//#######################################################################
+int  Fl_Tbl_Xcpot2::getMemory(void)
+{
+    int intI;
+    //TblFile = new char[Maxfilename];
+    //assert(TblFile != 0);
+
+    BsTbl = new BasisTable[cGtoTotalNum];
+    assert(BsTbl != 0);
+
+    for (intI=0; intI<cGtoTotalNum; intI++) {
+        BsTbl[intI].Atom  = new char[MaxAtomName];
+        assert(BsTbl[intI].Atom != 0);
+        BsTbl[intI].shell = new char[MaxLabelname];
+        assert(BsTbl[intI].shell != 0);
+        BsTbl[intI].dumy1 = new char[MaxLabelname];
+        assert(BsTbl[intI].dumy1 != 0);
+        BsTbl[intI].dumy2 = new char[MaxLabelname];
+        assert(BsTbl[intI].dumy2 != 0);
+    }
+
+    return 0;
+}
+//#######################################################################
+int Fl_Tbl_Xcpot2::makeTable(void)
+{
+    TlLogging& log = TlLogging::getInstance();
+
+    //const Fl_Geometry FlGeom((*this->pPdfParam_)["coordinates"]);
+    Fl_Gto_Xcpot2   FlGtoXcpot2;
+    TlPrdctbl        TPobj;    //これは、使うか？;
+
+    int i,j,k,m,t,rept,start,to,cGtonum,flag;
+    int basiscount;
+//  char Atm[MaxAtomName],Lb2[MaxLabelname];
+    char *Atm = new char[MaxAtomName];
+    char *Lb2 = new char[MaxLabelname];
+//  char *Atm,*Lb2;
+    char shell;
+
+    static const char* Sorb[20]={"s()"};
+    static const char* Porb[20]={"p(x)","p(y)","p(z)"};
+    static const char* Dorb[20]={"d(xy)","d(xz)","d(yz)","d(x2-y2)","d(3z2-r2)"};
+
+//   FlGeom.load();
+//   FlGeom.open("fl_Geometry","read");
+//   FlGeom.read();
+// //FlGeom.show();
+//   FlGeom.close();
+
+///// modified by Fric(mizouchi) 2001/12/09 (s) /////
+//    FlGtoXcpot2.open("fl_Gto_Xcpot2","read");
+//   FlGtoXcpot2.open("fl_Gto_Xcpot","read");
+///// modified by Fric(mizouchi) 2001/12/09 (e) /////
+
+//   FlGtoXcpot2.read();
+//FlGtoXcpot2.show();
+//   FlGtoXcpot2.close();
+
+
+    std::ofstream fo;
+///// modified by Fric(mizouchi) 2001/12/09 (s) /////
+//  TblFile = "fl_Table/Xcpot2Table";
+    TblFile = "fl_Table/XcpotTable2";
+///// modified by Fric(mizouchi) 2001/12/09 (e) /////
+
+    fo.open(TblFile.c_str(), std::ios::out | std::ios::trunc);
+    if (!fo) {
+        log.error("Cannot Open " + TblFile);
+        CnErr.abort();
+    }
+
+    basiscount = 0;
+    for (i=0; i<AtomNum; i++) {
+        flag=0;
+        strcpy(Atm, this->flGeom_.getAtom(i).c_str());
+        strcpy(Lb2, this->flGeom_.getLabel(i).c_str());
+        for (j=0; j<AtomKindNum; j++) {
+            m = FlGtoXcpot2.getStartposition(j);
+            if ((strcmp(Atm,FlGtoXcpot2.getAtom(m).c_str())==0)  &&
+                    (strcmp(Lb2,FlGtoXcpot2.getLabel(m).c_str())==0)) {
+                start = m;
+                to    = m + FlGtoXcpot2.getTermnumber(j);
+                for (k=start; k<to; k++) {
+                    cGtonum = k;
+                    shell   = FlGtoXcpot2.getShell(k);
+                    int iter = 0;
+                    switch (shell) {
+                    case 's' :
+                        iter=1;
+                        break;
+                    case 'p' :
+                        iter=3;
+                        break;
+                    case 'd' :
+                        iter=5;
+                        break;
+                    default  :
+                        log.error("Error (making Table for Yahito(Xpot2))!!");
+                        CnErr.abort();
+                        break;
+                    }
+                    rept=0;
+                    for (t=0; t<iter; t++) {
+                        fo << std::setw(8) <<  basiscount
+                        << std::setw(6) <<  cGtonum
+                        << std::setw(6) <<     i
+                        << std::setw(6) <<    Atm      <<"    " ;
+                        basiscount++;
+                        switch (shell) {
+                        case 's' :
+                            fo <<" s    "
+                            << std::setw(10) << Sorb[rept]
+                            <<"   ["<< Lb2<<"]"<< "\n";
+                            break;
+                        case 'p' :
+                            fo <<" p    "
+                            << std::setw(10) << Porb[rept]
+                            <<"   ["<<Lb2<<"]"<< "\n";
+                            break;
+                        case 'd' :
+                            fo <<" d    "
+                            << std::setw(10) << Dorb[rept]
+                            <<"   ["<<Lb2<<"]"<< "\n";
+                            break;
+                        default  :
+                            log.error("error(making Table for Yahiro(Xcpot2))");
+                            CnErr.abort();
+                            break;
+                        }       //switch(shell);
+                        rept++;
+                    }         //for( t );
+                }           //for( k );
+                flag=1;
+            }     //if;
+            if (flag==1) break;
+        }   //for( j );
+    }    //for( i );
+
+    fo.close();
+    setData();
+
+    delete[] Atm;
+    delete[] Lb2;
+
+    std::cerr << "Fl_Tbl_Xcpot2::makeTable() exit" << std::endl;
+    return 0;
+}
+//#########################################################################
+int  Fl_Tbl_Xcpot2::setData(void)
+{
+    TlLogging& log = TlLogging::getInstance();
+    int intI;
+    std::ifstream fi;
+    int inttmp;
+
+    TblFile = "fl_Table/XcpotTable2";
+
+    fi.open(TblFile.c_str(), std::ios::in);
+    if (!fi) {
+        log.error("Cannot open " + TblFile);
+        CnErr.abort();
+    }
+
+    for (intI=0; intI<cGtoTotalNum; intI++) {
+        fi >>inttmp >> BsTbl[intI].cGtonum >> BsTbl[intI].InpAtomnumber
+           >> BsTbl[intI].Atom       >> BsTbl[intI].shell
+           >> BsTbl[intI].dumy1      >> BsTbl[intI].dumy2     ;
+    }
+
+    fi.close();
+
+    return 0;
+
+}
+
+//#########################################################################
