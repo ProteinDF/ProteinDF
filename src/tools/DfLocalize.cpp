f435bab97a1d1140024ebec68da15745ca3ac499
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2021-06-19 22:29:10 +0900

    WIP

--- src/tools/DfLocalize.cpp
+++ src/tools/DfLocalize.cpp
@@ -177,16 +177,8 @@ double DfLocalize::localize(TlDenseGeneralMatrix_Lapack* pC) {
             sumDeltaG += deltaG;
             TlDenseGeneralMatrix_Lapack rot(2, 2);
             this->getRotatingMatrix(A_ij, B_ij, normAB, &rot);
-            // std::cout << rot << std::endl;
-            // const double det = rot.get(0, 0) * rot.get(1, 1) - rot.get(0, 1) * rot.get(1, 0);
-            // std::cout << "Det = " << det << std::endl;
-            // assert(std::fabs(det - 1.0) < 1.0E-5);
             this->rotateVectors(&Cpi, &Cpj, rot);
 
-            // for (index_type ao = 0; ao < numOfAOs; ++ao) {
-            //     pC->set(ao, orb_i, Cpi.get(ao));
-            //     pC->set(ao, orb_j, Cpj.get(ao));
-            // }
             pC->setColVector(orb_i, Cpi.data(), numOfAOs);
             pC->setColVector(orb_j, Cpj.data(), numOfAOs);
         }
@@ -202,8 +194,6 @@ void DfLocalize::makeGroup() {
 
     const int numOfGroups = numOfAtoms;
 
-    // this->group_.clear();
-    // this->group_.resize(numOfGroups);
     this->groupV_.clear();
     this->groupV_.resize(numOfGroups);
     for (int i = 0; i < numOfGroups; ++i) {
@@ -212,79 +202,9 @@ void DfLocalize::makeGroup() {
 
     for (index_type orb = 0; orb < numOfAOs; ++orb) {
         const index_type atomID = this->orbInfo_.getAtomIndex(orb);
-        // this->group_[atomID].push_back(orb);
-
         this->groupV_[atomID].set(orb, 1.0);
     }
 }
-
-// void DfLocalize::localize(const std::string& inputCMatrixPath, const int restartIteration) {
-//     std::cout << TlUtils::format("number of Atoms = %ld", this->m_nNumOfAtoms) << std::endl;
-//     std::cout << TlUtils::format("number of AOs   = %ld", this->m_nNumOfAOs) << std::endl;
-//     std::cout << TlUtils::format("number of MOs   = %ld", this->m_nNumOfMOs) << std::endl;
-//     this->makeGroup();
-
-//     const std::string S_path = this->getSpqMatrixPath();
-//     if (this->S_.load(S_path) == false) {
-//         std::cerr << "could not load: " << S_path << std::endl;
-//         abort();
-//     }
-
-//     std::string CMatrixPath = inputCMatrixPath;
-//     if (CMatrixPath.empty() == true) {
-//         CMatrixPath = this->getCMatrixPath(DfObject::RUN_RKS, this->m_nIteration);
-//     }
-//     if (this->C_.load(CMatrixPath) == false) {
-//         std::cerr << "could not load: " << CMatrixPath << std::endl;
-//         abort();
-//     }
-
-//     const int maxIteration = this->maxIteration_;
-//     std::cout << TlUtils::format("max iteration = %d", maxIteration) << std::endl;
-//     std::cout << TlUtils::format("threshold = %10.5e", this->threshold_) << std::endl;
-
-//     int startIteration = std::max(0, restartIteration);
-//     if (restartIteration > 0) {
-//         this->C_ = DfObject::getCloMatrix<TlDenseGeneralMatrix_Lapack>(DfObject::RUN_RKS, startIteration + 1);
-//     }
-
-//     std::cerr << TlUtils::format("start iteration=%d", startIteration + 1) << std::endl;
-//     for (int num_iteration = startIteration; num_iteration < maxIteration; ++num_iteration) {
-//         this->makeQATable(C);
-
-//         this->makeJobList();
-
-//         double sumDeltaG = 0.0;
-//         JobItem jobItem;
-//         (void)this->getJobItem(&jobItem, true);
-//         while (this->getJobItem(&jobItem) != 0) {
-//             const std::size_t orb_i = jobItem.orb_i;
-//             const std::size_t orb_j = jobItem.orb_j;
-
-//             double A_ij, B_ij;
-//             this->calcQA(this->C_, orb_i, orb_j, &A_ij, &B_ij);
-//             double normAB = std::sqrt(A_ij * A_ij + B_ij * B_ij);
-//             double deltaG = A_ij + normAB;
-
-//             if (std::fabs(deltaG) > 1.0E-16) {
-//                 sumDeltaG += deltaG;
-//                 TlDenseGeneralMatrix_Lapack rot(2, 2);
-//                 this->getRotatingMatrix(A_ij, B_ij, normAB, &rot);
-//                 this->rotateCmatrix(orb_i, orb_j, rot);
-//             }
-//         }
-
-//         std::cout << TlUtils::format("%d th: sum of delta_g: %10.5e", num_iteration + 1, sumDeltaG) << std::endl;
-//         DfObject::saveCloMatrix(RUN_RKS, num_iteration + 1, this->C_);
-
-//         if (sumDeltaG < this->threshold_) {
-//             std::cout << "number of iteration: " << num_iteration + 1 << std::endl;
-//             (*(this->pPdfParam_))["lo/num_of_iterations"] = num_iteration + 1;
-//             (*(this->pPdfParam_))["lo/satisfied"] = "yes";
-//             break;
-//         }
-//     }
-// }
 
 void DfLocalize::makeQATable(const TlDenseGeneralMatrix_Lapack& C) {
     this->log_.info("make QA table");
@@ -298,13 +218,8 @@ void DfLocalize::makeQATable(const TlDenseGeneralMatrix_Lapack& C) {
     this->orb_QA_table_.resize(size);
 #pragma omp parallel for schedule(runtime) reduction(+ : sumQAii2)
     for (index_type i = 0; i < size; ++i) {
-        // #ifdef _OPENMP
-        //         this->log_.info(TlUtils::format("maktQATable: %d/%d @%d", i, size, omp_get_thread_num()));
-        // #endif  // _OPENMP
-
         const index_type orb = startOrb + i;
         const double QAii2 = this->calcQA_ii(C, orb);
-        // std::cout << TlUtils::format("Q_Ai^2 = %16.8e", QAii2) << std::endl;
         sumQAii2 += QAii2;
         Orb_QA_Item item(orb, QAii2);
         this->orb_QA_table_[i] = item;
@@ -318,23 +233,6 @@ void DfLocalize::makeQATable(const TlDenseGeneralMatrix_Lapack& C) {
     this->log_.info("end QA table");
 }
 
-// void DfLocalize::rotateCmatrix(TlDenseGeneralMatrix_Lapack* pC, std::size_t orb_i, std::size_t orb_j,
-//                                const TlDenseGeneralMatrix_Lapack& rot) {
-//     const std::size_t numOfAOs = this->m_nNumOfAOs;
-//     TlDenseGeneralMatrix_Lapack A(2, numOfAOs);
-//     for (std::size_t i = 0; i < numOfAOs; ++i) {
-//         A.set(0, i, pC->get(i, orb_i));
-//         A.set(1, i, pC->get(i, orb_j));
-//     }
-
-//     const TlDenseGeneralMatrix_Lapack B = rot * A;  // rotate A to form B
-
-//     for (std::size_t i = 0; i < numOfAOs; ++i) {
-//         pC->set(i, orb_i, B.get(0, i));
-//         pC->set(i, orb_j, B.get(1, i));
-//     }
-// }
-
 void DfLocalize::rotateVectors(TlDenseVector_Lapack* pCpi, TlDenseVector_Lapack* pCpj,
                                const TlDenseGeneralMatrix_Lapack& rot) {
     const index_type numOfAOs = this->m_nNumOfAOs;
@@ -344,10 +242,6 @@ void DfLocalize::rotateVectors(TlDenseVector_Lapack* pCpi, TlDenseVector_Lapack*
     TlDenseGeneralMatrix_Lapack A(2, numOfAOs);
     A.setRowVector(0, pCpi->data(), numOfAOs);
     A.setRowVector(1, pCpj->data(), numOfAOs);
-    // for (index_type i = 0; i < numOfAOs; ++i) {
-    //     A.set(0, i, pCpi->get(i));
-    //     A.set(1, i, pCpj->get(i));
-    // }
 
     const TlDenseGeneralMatrix_Lapack B = rot * A;  // rotate A to form B
 
@@ -382,76 +276,10 @@ void DfLocalize::getRotatingMatrix(const double A_ij, const double B_ij, const d
     pRot->set(1, 1, cos_a);
 }
 
-// void DfLocalize::calcQA_ij(const TlDenseGeneralMatrix_Lapack& C, const std::size_t orb_i, const std::size_t orb_j,
-//                            double* pA_ij, double* pB_ij) {
-//     assert(pA_ij != NULL);
-//     assert(pB_ij != NULL);
-//     assert(C.getNumOfRows() == this->m_nNumOfAOs);
-//     assert(C.getNumOfCols() == this->m_nNumOfMOs);
-//     assert(this->S_.getNumOfRows() == this->m_nNumOfAOs);
-//     assert(this->S_.getNumOfCols() == this->m_nNumOfAOs);
-
-//     const index_type numOfAOs = this->m_nNumOfAOs;
-//     const index_type numOfGrps = this->group_.size();
-
-//     TlDenseGeneralMatrix_Lapack Cpi(numOfAOs, 1);
-//     C.block(0, orb_i, numOfAOs, 1, &Cpi);
-//     TlDenseGeneralMatrix_Lapack Cpj(numOfAOs, 1);
-//     C.block(0, orb_j, numOfAOs, 1, &Cpj);
-
-//     const TlDenseGeneralMatrix_Lapack SCpi = this->S_ * Cpi;
-//     const TlDenseGeneralMatrix_Lapack SCpj = this->S_ * Cpj;
-
-//     double sumAij = 0.0;
-//     double sumBij = 0.0;
-
-// #pragma omp parallel for
-//     for (std::size_t grp = 0; grp < numOfGrps; ++grp) {
-//         const std::vector<index_type>& groupAOs = this->group_[grp];
-//         const index_type numOfGroupAOs = groupAOs.size();
-//         if (numOfGroupAOs == 0) {
-//             continue;
-//         }
-
-//         TlDenseGeneralMatrix_Lapack tCqi(numOfAOs, 1);
-//         TlDenseGeneralMatrix_Lapack tCqj(numOfAOs, 1);
-//         for (index_type k = 0; k < numOfGroupAOs; ++k) {
-//             const index_type ao = groupAOs[k];
-//             tCqi.set(0, ao, Cpi.get(ao, 0));
-//             tCqj.set(0, ao, Cpj.get(ao, 0));
-//         }
-
-//         const TlDenseGeneralMatrix_Lapack QAiiMat = tCqi * SCpi;
-//         assert(QAiiMat.getNumOfRows() == 1);
-//         assert(QAiiMat.getNumOfCols() == 1);
-//         const double QAii = QAiiMat.get(0, 0);
-
-//         const TlDenseGeneralMatrix_Lapack QAjjMat = tCqj * SCpj;
-//         assert(QAjjMat.getNumOfRows() == 1);
-//         assert(QAjjMat.getNumOfCols() == 1);
-//         const double QAjj = QAjjMat.get(0, 0);
-
-//         const TlDenseGeneralMatrix_Lapack QAijMat = tCqj * SCpi + tCqi * SCpj;
-//         assert(QAijMat.getNumOfRows() == 1);
-//         assert(QAijMat.getNumOfCols() == 1);
-//         const double QAij = 0.5 * QAijMat.get(0, 0);
-
-//         const double QAii_QAjj = QAii - QAjj;
-//         sumAij += QAij * QAij - 0.25 * QAii_QAjj * QAii_QAjj;
-//         sumBij += QAij * QAii_QAjj;
-//     }
-
-//     *pA_ij = sumAij;
-//     *pB_ij = sumBij;
-// }
-
 void DfLocalize::calcQA_ij(const TlDenseVector_Lapack& Cpi, const TlDenseVector_Lapack& Cpj, double* pA_ij,
                            double* pB_ij) {
     assert(pA_ij != NULL);
     assert(pB_ij != NULL);
-
-    // std::cout << Cpi << std::endl;
-    // std::cout << Cpj << std::endl;
 
     const index_type numOfAOs = this->m_nNumOfAOs;
     const index_type numOfGrps = this->groupV_.size();
@@ -464,21 +292,6 @@ void DfLocalize::calcQA_ij(const TlDenseVector_Lapack& Cpi, const TlDenseVector_
 
 #pragma omp parallel for schedule(runtime) reduction(+ : sumAij, sumBij)
     for (index_type grp = 0; grp < numOfGrps; ++grp) {
-        // const std::vector<index_type>& groupAOs = this->group_[grp];
-        // const index_type numOfGroupAOs = groupAOs.size();
-        // if (numOfGroupAOs == 0) {
-        //     continue;
-        // }
-
-        // TlDenseVector_Lapack tCqi(numOfAOs);
-        // TlDenseVector_Lapack tCqj(numOfAOs);
-        // for (index_type k = 0; k < numOfGroupAOs; ++k) {
-        //     const index_type ao = groupAOs[k];
-        //     assert(0 <= ao);
-        //     assert(ao < numOfAOs);
-        //     tCqi.set(ao, Cpi.get(ao));
-        //     tCqj.set(ao, Cpj.get(ao));
-        // }
         TlDenseVector_Lapack tCqi = Cpi;
         TlDenseVector_Lapack tCqj = Cpj;
         tCqi.dotInPlace(this->groupV_[grp]);
@@ -486,13 +299,9 @@ void DfLocalize::calcQA_ij(const TlDenseVector_Lapack& Cpi, const TlDenseVector_
 
         const double QAii = tCqi * SCpi;
         const double QAjj = tCqj * SCpj;
-        // const double QAij1 = tCqj * SCpi;
-        // const double QAij2 = tCqi * SCpj;
         const double QAij = 0.5 * (tCqj * SCpi + tCqi * SCpj);
 
         const double QAii_QAjj = QAii - QAjj;
-        // std::cout << TlUtils::format("QAii=%f, QAjj=%f, QAij=%f, QAij1=%f, QAij2=%f", QAii, QAjj, QAij, QAij1, QAij2)
-        // << std::endl;
         sumAij += QAij * QAij - 0.25 * QAii_QAjj * QAii_QAjj;
         sumBij += QAij * QAii_QAjj;
     }
@@ -506,40 +315,15 @@ double DfLocalize::calcQA_ii(const TlDenseGeneralMatrix_Lapack& C, const index_t
     const index_type numOfGrps = this->groupV_.size();
     double sumQAii2 = 0.0;
 
-    // TlDenseGeneralMatrix_Lapack Cpi(numOfAOs, 1);
-    // C.block(0, orb_i, numOfAOs, 1, &Cpi);
-    // const TlDenseGeneralMatrix_Lapack SCpi = this->S_ * Cpi;
     const TlDenseVector_Lapack Cpi = C.getColVector<TlDenseVector_Lapack>(orb_i);
     const TlDenseVector_Lapack SCpi = this->S_ * Cpi;
 
     // #pragma omp parallel for reduction(+ : sumQAii2)
     for (index_type grp = 0; grp < numOfGrps; ++grp) {
-        // const std::vector<index_type>& groupAOs = this->group_[grp];
-        // const index_type numOfGroupAOs = groupAOs.size();
-        // if (numOfGroupAOs == 0) {
-        //     continue;
-        // }
-
-        // TlDenseGeneralMatrix_Lapack tCqi(1, numOfAOs);
-        // for (index_type k = 0; k < numOfGroupAOs; ++k) {
-        //     const index_type ao = groupAOs[k];
-        //     tCqi.set(0, ao, Cpi.get(ao, 0));
-        // }
-        // TlDenseVector_Lapack tCqi(numOfAOs);
-        // for (index_type k = 0; k < numOfGroupAOs; ++k) {
-        //     const index_type ao = groupAOs[k];
-        //     tCqi.set(ao, Cpi.get(ao));
-        // }
-
         TlDenseVector_Lapack tCqi = Cpi;
         tCqi.dotInPlace(this->groupV_[grp]);
 
-        // const TlDenseGeneralMatrix_Lapack QAiiMat = tCqi * SCpi;
-        // assert(QAiiMat.getNumOfRows() == 1);
-        // assert(QAiiMat.getNumOfCols() == 1);
-        // const double QAii = QAiiMat.get(0, 0);
         const double QAii = tCqi * SCpi;
-
         sumQAii2 += QAii * QAii;
     }
 
@@ -561,17 +345,6 @@ void DfLocalize::makeJobList() {
             ++index;
         }
     }
-
-    // std::vector<Orb_QA_Item>::const_iterator itrEnd = this->orb_QA_table_.end();
-    // for (std::vector<Orb_QA_Item>::const_iterator p = this->orb_QA_table_.begin(); p != itrEnd; ++p) {
-    //     item.orb_i = p->orb;
-
-    //     for (std::vector<Orb_QA_Item>::const_iterator q = p + 1; q != itrEnd; ++q) {
-    //         item.orb_j = q->orb;
-    //         this->jobList_.push_back(item);
-    //         ++index;
-    //     }
-    // }
 }
 
 bool DfLocalize::getJobItem(DfLocalize::JobItem* pJob, bool isInitialized) {
