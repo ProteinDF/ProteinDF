dfb71791e56db16baf76d0dc5ab62c6d25d10ab4
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2022-02-12 11:46:19 +0900

    feat: TlFile member functions
    TlFile::dir(), filename(), ::stem(), and extension()

--- src/unit_test/TlFileTest.cpp
+++ src/unit_test/TlFileTest.cpp
@@ -1,7 +1,27 @@
 #include "TlFile.h"
 #include "gtest/gtest.h"
 
-TEST(TlFIle, isExistFile) {
+TEST(TlFile, dir) {
+    const std::string dir = TlFile::dir("/home/abc/def.ghi");
+    EXPECT_EQ(std::string("/home/abc"), dir);
+}
+
+TEST(TlFile, filename) {
+    const std::string filename = TlFile::filename("/home/abc/def.ghi");
+    EXPECT_EQ(std::string("def.ghi"), filename);
+}
+
+TEST(TlFile, stem) {
+    const std::string stem = TlFile::stem("/home/abc/def.ghi");
+    EXPECT_EQ(std::string("def"), stem);
+}
+
+TEST(TlFile, extension) {
+    const std::string ext = TlFile::extension("/home/abc/def.ghi");
+    EXPECT_EQ(std::string(".ghi"), ext);
+}
+
+TEST(TlFile, isExistFile) {
     bool a = TlFile::isExistFile("/bin/bash");
     bool b = TlFile::isExistFile("unexisted_file");
     // bool c = TlFile::isExistFile("/tmp");
