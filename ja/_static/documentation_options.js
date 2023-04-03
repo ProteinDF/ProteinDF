0985f54049fada5435e38bfafa21cbd7b7df0b33
hiracchi <hiracchi@users.noreply.github.com>
2023-04-03 10:56:23 +0000

    deploy: cda08a3d44e5255d821b82e40c88f85399e14cf6

--- ja/_static/documentation_options.js
+++ ja/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2022.2.1',
+    VERSION: '2023.3.5',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
